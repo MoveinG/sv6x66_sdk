dcb7faf849b46d5a723c02c3a53abe93472f9700
YJZuo <zyongjun@waterworld.com.cn>
2018-11-14 14:34:46 +0800

    download uart(bootloader) 115200 -> 921600

--- projects/uart_echo/src/bootloader/lib/serial.h
+++ projects/uart_echo/src/bootloader/lib/serial.h
@@ -2,11 +2,12 @@
 #define _SERIAL_H
 
 #include "soc_defs.h"
+#include "attrs.h"
 
-void serial_init(void);
-void serial_tx(int);
-int serial_rx_ready(void);
-int serial_rx(void);
+void serial_init(int BaudRate);
+void serial_tx(int) ATTRIBUTE_SECTION_OTA_FBOOT;
+int serial_rx_ready(void) ATTRIBUTE_SECTION_OTA_FBOOT;
+int serial_rx() ATTRIBUTE_SECTION_OTA_FBOOT;
 
 void serial_uart1_init(int BaudRate);
 void serial_uart1_tx(int);
